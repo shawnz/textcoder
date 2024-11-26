45adaae1a25330487de28e7463273b918e28e45a
Shawn Zivontsis <shawnz@shawnz.org>
2024-11-26 10:39:34 -0500

    Merge decoder changes

--- mtimmerm/biacode/src/python/anthropic/biacode.py
+++ mtimmerm/biacode/src/python/anthropic/biacode.py
@@ -1,11 +1,12 @@
 import sys
 import argparse
 from io import BytesIO
+from arithmetic_decoder import ArithmeticDecoder
 from arithmetic_encoder import ArithmeticEncoder
 from simple_model import SimpleAdaptiveModel
 
 
-class FOBitStream:
+class FOBitOutputStream:
     def __init__(self, stream, block_size=1):
         self.stream = stream
         self.block_size = max(1, block_size)
@@ -31,9 +32,48 @@ class FOBitStream:
             self.block_left -= 1
 
 
+class FOBitInputStream:
+    def __init__(self, stream, block_size=1):
+        self.stream = stream
+        self.block_size = max(1, block_size)
+        self.reserve0 = False
+        self.block_left = 0
+        self.in_done = False
+
+    def read(self, size=1):
+        if size != 1:
+            raise ValueError("This stream only supports reading 1 byte at a time")
+
+        while True:
+            if self.in_done:
+                return b"\x00"
+
+            byte = self.stream.read(1)
+            if not byte:
+                self.in_done = True
+                return b"\x00"
+
+            byte = byte[0] ^ 0x55  # XOR with 55 for de-obfuscation
+
+            if self.block_left:
+                self.reserve0 = self.reserve0 and not byte
+                self.block_left -= 1
+                return bytes([byte])
+
+            # Start of a new block
+            if self.in_done:
+                if self.reserve0:
+                    return b"\x80"  # End marker
+                break
+
+            self.reserve0 = not byte if not self.reserve0 else not (byte & 0x7F)
+            self.block_left = self.block_size - 1
+            return bytes([byte])
+
+
 def compress(input_stream, output_stream, block_size=1):
     model = SimpleAdaptiveModel(256)
-    encoder = ArithmeticEncoder(FOBitStream(output_stream, block_size))
+    encoder = ArithmeticEncoder(FOBitOutputStream(output_stream, block_size))
 
     while True:
         byte = input_stream.read(1)
@@ -49,15 +89,13 @@ def compress(input_stream, output_stream, block_size=1):
 
 def decompress(input_stream, output_stream, block_size=1):
     model = SimpleAdaptiveModel(256)
-    # For this version, we'll use BytesIO as a bit stream simulator
-    # In a real implementation, you'd create a more robust bit input stream
+    decoder = ArithmeticDecoder(FOBitInputStream(input_stream, block_size))
 
     while True:
-        sym = input_stream.read(1)
-        if not sym:
+        sym = decoder.decode(model, True)
+        if sym < 0:
             break
 
-        sym = sym[0]
         output_stream.write(bytes([sym]))
         model.update(sym)
 
