fcb8dac127a802352f0a672f2956ebf12d9ceee8
Shawn Zivontsis <shawnz@shawnz.org>
2024-11-26 09:41:17 -0500

    Add decoder class

---
+++ mtimmerm/biacode/src/python/arithmetic_decoder.py
@@ -0,0 +1,85 @@
+class ArithmeticDecoder:
+    BIT16 = 65536
+    MASK16 = 0xFFFF
+
+    def __init__(self, input_stream):
+        self.bytesin = input_stream
+        self.low = 0
+        self.range = self.BIT16
+        self.interval_bits = 16
+        self.free_end_even = self.MASK16
+        self.next_free_end = 0
+        self.value = 0
+        self.value_shift = -24
+        self.follow_byte = 0
+        self.follow_buf = 1
+
+    def decode(self, model, can_end=False):
+        # Refill value buffer
+        while self.value_shift <= 0:
+            self.value <<= 8
+            self.value_shift += 8
+            
+            if not self.follow_buf:
+                self.value |= self.follow_byte
+                
+                # Refill follow buffer
+                try:
+                    self.follow_byte = self.bytesin.read(1)[0]
+                    self.follow_buf = 1
+                except (IndexError, TypeError):
+                    self.follow_buf = -1
+                    self.follow_byte = 0
+
+        # Check for end of stream
+        if can_end:
+            if (self.follow_buf < 0 and 
+                (((self.next_free_end - self.low) << self.value_shift) == self.value)):
+                return -1  # EOF
+            
+            if self.next_free_end:
+                self.next_free_end += (self.free_end_even + 1) * 2
+            else:
+                self.next_free_end = self.free_end_even + 1
+
+        # Decode symbol
+        newl = ((self.value >> self.value_shift) * model.prob_one() + 
+                model.prob_one() - 1) // self.range
+        
+        symbol, sym_low, sym_high = model.get_symbol(newl)
+        
+        newl = sym_low * self.range // model.prob_one()
+        newh = sym_high * self.range // model.prob_one()
+
+        self.range = newh - newl
+        self.value -= (newl << self.value_shift)
+        self.low += newl
+
+        # Adjust nextfreeend
+        if self.next_free_end < self.low:
+            self.next_free_end = ((self.low + self.free_end_even) & ~self.free_end_even) | (self.free_end_even + 1)
+
+        # Range scaling
+        if self.range <= (self.BIT16 >> 1):
+            self.low *= 2
+            self.range *= 2
+            self.next_free_end *= 2
+            self.free_end_even = self.free_end_even * 2 + 1
+            self.value_shift -= 1
+
+            while self.next_free_end - self.low >= self.range:
+                self.free_end_even //= 2
+                self.next_free_end = ((self.low + self.free_end_even) & ~self.free_end_even) | (self.free_end_even + 1)
+
+            while self.interval_bits < 24:
+                if self.range > (self.BIT16 >> 1):
+                    break
+
+                self.low *= 2
+                self.range *= 2
+                self.next_free_end *= 2
+                self.free_end_even = self.free_end_even * 2 + 1
+                self.value_shift -= 1
+                self.interval_bits += 8
+
+        return symbol
---
+++ mtimmerm/biacode/src/python/biacode_updated.py
@@ -0,0 +1,73 @@
+import sys
+import argparse
+from io import BytesIO
+from arithmetic_encoder import ArithmeticEncoder
+from arithmetic_decoder import ArithmeticDecoder
+from simple_model import SimpleAdaptiveModel
+
+class FOBitInputStream:
+    def __init__(self, stream, block_size=1):
+        self.stream = stream
+        self.block_size = max(1, block_size)
+        self.reserve0 = False
+        self.block_left = 0
+        self.in_done = False
+
+    def read(self, size=1):
+        if size != 1:
+            raise ValueError("This stream only supports reading 1 byte at a time")
+        
+        while True:
+            if self.in_done:
+                return b'\x00'
+            
+            byte = self.stream.read(1)
+            if not byte:
+                self.in_done = True
+                return b'\x00'
+            
+            byte = byte[0] ^ 0x55  # XOR with 55 for de-obfuscation
+            
+            if self.block_left:
+                self.reserve0 = self.reserve0 and not byte
+                self.block_left -= 1
+                return bytes([byte])
+            
+            # Start of a new block
+            if self.in_done:
+                if self.reserve0:
+                    return b'\x80'  # End marker
+                break
+            
+            self.reserve0 = not byte if not self.reserve0 else not(byte & 0x7F)
+            self.block_left = self.block_size - 1
+            return bytes([byte])
+
+def compress(input_stream, output_stream, block_size=1):
+    model = SimpleAdaptiveModel(256)
+    encoder = ArithmeticEncoder(FOBitOutputStream(output_stream, block_size))
+    
+    while True:
+        byte = input_stream.read(1)
+        if not byte:
+            break
+        
+        sym = byte[0]
+        encoder.encode(model, sym, True)
+        model.update(sym)
+    
+    encoder.end()
+
+def decompress(input_stream, output_stream, block_size=1):
+    model = SimpleAdaptiveModel(256)
+    decoder = ArithmeticDecoder(FOBitInputStream(input_stream, block_size))
+    
+    while True:
+        sym = decoder.decode(model, True)
+        if sym < 0:
+            break
+        
+        output_stream.write(bytes([sym]))
+        model.update(sym)
+
+# Rest of the script remains the same as in the previous version
