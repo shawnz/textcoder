fcbcbf0a87db6ad78a9e02eac0c9e1b62011e2ed
Shawn Zivontsis <shawnz@shawnz.org>
2025-01-20 13:42:30 -0500

    Add encryption

--- mtimmerm/biacode/src/python/anthropic/llm_model.py
+++ mtimmerm/biacode/src/python/anthropic/llm_model.py
@@ -1,12 +1,30 @@
+import time
+
 import torch
+import transformers
 
 _TARGET_PROB_ONE = 32768
+_TOP_K = 40
 
 
 class LLMModel:
-    def __init__(self, model, initial_input):
+    def __init__(self, model, initial_input, special_token_ids):
         self._model = model
-        self._input = initial_input
+        self._special_token_ids = special_token_ids
+        self._past_key_values = transformers.DynamicCache()
+        # self._past_key_values = transformers.StaticCache(
+        #     config=model.config,
+        #     batch_size=1,
+        #     # If you plan to reuse the cache, make sure the cache length is large enough for all cases
+        #     max_cache_len=(len(initial_input.input_ids) + 1000),
+        #     device=model.device,
+        #     dtype=model.dtype,
+        # )
+        print(f"{time.time()}: started initial forward pass")
+        self._logits = model(
+            **initial_input, past_key_values=self._past_key_values
+        ).logits[0, -1]
+        print(f"{time.time()}: done initial forward pass")
         self._probs_dirty = True
         self._sorted_tok_ids_dirty = True
 
@@ -16,34 +34,61 @@ class LLMModel:
         return torch.cat((tensor, tail), dim=1)
 
     def _recompute_probs(self):
-        logits = self._model(**self._input).logits[0, -1]
-        sorted_decimal_probs, sorted_decimal_prob_indices = torch.sort(
-            torch.softmax(logits, dim=0), descending=True
-        )
+        print(f"{time.time()}: started recomputing probs")
+        # logits = self._model(
+        #     **self._input, past_key_values=self._past_key_values
+        # ).logits[0, -1]
+        # print(f"{time.time()}: ran the model")
+        filtered_logits = self._logits
+        filtered_logits[self._special_token_ids] = float("-inf")
+        if _TOP_K == 0:
+            sorted_decimal_probs, sorted_decimal_prob_indices = torch.sort(
+                torch.softmax(filtered_logits, dim=0), descending=True
+            )
+        else:
+            topk_logits, topk_logit_indices = torch.topk(filtered_logits, _TOP_K)
+            sorted_decimal_probs, sorted_decimal_prob_indices = (
+                torch.softmax(topk_logits, dim=0),
+                topk_logit_indices,
+            )
+        print(f"{time.time()}: sorted softmax logits")
         scaled_int_probs = torch.ceil(sorted_decimal_probs * _TARGET_PROB_ONE).int()
+        print(f"{time.time()}: scaled and rounded probs to ints")
         cumsum_probs = torch.cumsum(scaled_int_probs, dim=0)
+        print(f"{time.time()}: computed cumsum probs")
         truncation_point = torch.searchsorted(cumsum_probs, _TARGET_PROB_ONE)
         self._probs = cumsum_probs[: truncation_point + 1]
         self._prob_tok_ids = sorted_decimal_prob_indices[: truncation_point + 1]
         self._probs_dirty = False
+        print(f"{time.time()}: done recomputing probs")
 
     def _recompute_sorted_tok_ids(self):
+        print(f"{time.time()}: started recomputing sorted tok ids")
         if self._probs_dirty:
             self._recompute_probs()
         self._sorted_tok_ids, self._sorted_tok_id_indices = torch.sort(
             self._prob_tok_ids
         )
         self._sorted_tok_ids_dirty = False
+        print(f"{time.time()}: done recomputing sorted tok ids")
 
     def update(self, token):
-        old_input_ids = self._input["input_ids"]
-        old_attention_mask = self._input["attention_mask"]
-        self._input = {
-            "input_ids": self._append_input_tensor(old_input_ids, token),
-            "attention_mask": self._append_input_tensor(old_attention_mask, 1),
-        }
+        print(f"{time.time()}: started update")
+        # print(f"{time.time()}: started appending token")
+        # old_input_ids = self._input["input_ids"]
+        # old_attention_mask = self._input["attention_mask"]
+        # self._input = {
+        #     "input_ids": self._append_input_tensor(old_input_ids, token),
+        #     "attention_mask": self._append_input_tensor(old_attention_mask, 1),
+        # }
+        self._logits = self._model(
+            input_ids=torch.tensor([[token]], device=self._model.device),
+            past_key_values=self._past_key_values,
+        ).logits[0, -1]
         self._probs_dirty = True
         self._sorted_tok_ids_dirty = True
+        # print(f"{time.time()}: done appending token")
+        print(f"{time.time()}: done update")
 
     def get_sym_range(self, symbol):
         if self._sorted_tok_ids_dirty:
--- mtimmerm/biacode/src/python/anthropic/textcoder.py
+++ mtimmerm/biacode/src/python/anthropic/textcoder.py
@@ -1,67 +1,151 @@
 import io
+import time
+import os
+import random
 
 from arithmetic_decoder import ArithmeticDecoder
 from arithmetic_encoder import ArithmeticEncoder
 from foio import FOBitInputStream, FOBitOutputStream
 from llm_model import LLMModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+from cryptography.hazmat.primitives.ciphers.aead import AESGCMSIV
+from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
+_MODEL = "meta-llama/Llama-3.2-1B-Instruct"
 _PROMPT = "You are a Twitter user, about to post a tweet. What does the tweet say? Please respond with just the tweet contents with no quotes."
 
 
 def compress(input_stream, output_stream, block_size=1):
-    tokenizer = AutoTokenizer.from_pretrained("gpt2")
-    model = AutoModelForCausalLM.from_pretrained("gpt2")
-    initial_input = tokenizer(_PROMPT, return_tensors="pt")
-    model = LLMModel(model, initial_input)
+    tokenizer = AutoTokenizer.from_pretrained(_MODEL)
+    model = AutoModelForCausalLM.from_pretrained(
+        _MODEL, torch_dtype="auto", device_map="auto"
+    )
+    initial_input = tokenizer(_PROMPT, return_tensors="pt").to(model.device)
+    model = LLMModel(model, initial_input, list(tokenizer.added_tokens_decoder.keys()))
     outbits = FOBitOutputStream(output_stream, block_size)
     encoder = ArithmeticEncoder(outbits)
 
     input_tokens = tokenizer(input_stream)
-    for input_token in input_tokens.input_ids:
+    input_toks_processed = []
+    for i, input_token in enumerate(input_tokens.input_ids):
+        if i == 0 and input_token == tokenizer.bos_token_id:
+            print(f"{time.time()}: skipping bos token")
+            continue
+
+        print(
+            f"{time.time()}: encoding token {input_token} ({repr(tokenizer.decode([input_token]))}, #{i} of {len(input_tokens)})"
+        )
         encoder.encode(model, input_token, True)
+        print(
+            f"{time.time()}: encoded token {input_token} ({repr(tokenizer.decode([input_token]))})"
+        )
+
+        input_toks_processed.append(input_token)
+        print(
+            f"{time.time()}: input processed so far: {repr(tokenizer.decode(input_toks_processed))}"
+        )
         model.update(input_token)
+        print(f"{time.time()}: updated model")
 
     encoder.end()
     outbits.end()
 
+    return input_toks_processed
+
 
 def decompress(input_stream, block_size=1):
-    tokenizer = AutoTokenizer.from_pretrained("gpt2")
-    model = AutoModelForCausalLM.from_pretrained("gpt2")
-    initial_input = tokenizer(_PROMPT, return_tensors="pt")
-    model = LLMModel(model, initial_input)
+    tokenizer = AutoTokenizer.from_pretrained(_MODEL)
+    model = AutoModelForCausalLM.from_pretrained(
+        _MODEL, torch_dtype="auto", device_map="auto"
+    )
+    initial_input = tokenizer(_PROMPT, return_tensors="pt").to(model.device)
+    model = LLMModel(model, initial_input, list(tokenizer.added_tokens_decoder.keys()))
     decoder = ArithmeticDecoder(FOBitInputStream(input_stream, block_size))
 
     output_toks = []
+    i = 0
     while True:
+        print(f"{time.time()}: decoding token #{i}")
+        i += 1
         sym = decoder.decode(model, True)
+        print(f"{time.time()}: decoded token {sym}")
         if sym < 0:
             break
+        print(f"{time.time()}: token {sym} has text {repr(tokenizer.decode([sym]))}")
 
         output_toks.append(sym)
+        print(
+            f"{time.time()}: complete output so far: {repr(tokenizer.decode(output_toks))}"
+        )
         model.update(sym)
+        print(f"{time.time()}: updated model")
 
-    return tokenizer.decode(output_toks)
+    return output_toks, tokenizer.decode(output_toks)
 
 
 def main():
+    print(f"{time.time()}: started script")
 
-    encoded = decompress(io.BytesIO(b"what if the string is really long? hello goodbye"))
+    #torch.use_deterministic_algorithms(True)
+    #torch.manual_seed(0)
+    #random.seed(0)
+
+    torch.mps.empty_cache()
+
+    password = b"password"
+    # plaintext = b"what if the string is really long? hello goodbye"
+    plaintext = b"plaintext"
+    associated_data = None
+    # salt = os.urandom(16)
+    salt = bytes.fromhex("57822ab590c8d4a08fcef398e810fd9a")
+    # nonce = os.urandom(12)
+    nonce = bytes.fromhex("3462db465be6182e7a285149")
+
+    kdf = Argon2id(salt=salt, length=32, iterations=1, lanes=4, memory_cost=64 * 1024)
+    key = kdf.derive(password)
+
+    aesgcmsiv = AESGCMSIV(key)
+    ct = aesgcmsiv.encrypt(nonce, plaintext, associated_data)
+    input_bytes = salt + nonce + ct
+
+    print(f"input_bytes=0x{input_bytes.hex()}")
+
+    output_toks, encoded = decompress(io.BytesIO(input_bytes))
+
+    print(f"input_bytes=0x{input_bytes.hex()}")
+    print(f"{output_toks=}")
+
+    torch.mps.empty_cache()
+
+    print(f"{time.time()}: decompressed input")
 
     print(f"{encoded=}")
 
     output_stream = io.BytesIO()
-    compress(encoded, output_stream)
+    input_toks_processed = compress(encoded, output_stream)
 
-    decoded_bytes = output_stream.getvalue()
-    
-    print(f"{decoded_bytes=}")
-    
+    print(f"{time.time()}: compressed output")
+
+    output_bytes = output_stream.getvalue()
+
+    print(f"{encoded=}")
+    print(f" input_bytes=0x{input_bytes.hex()}")
+    print(f"output_bytes=0x{output_bytes.hex()}")
+    print(f"         {output_toks=}")
+    print(f"{input_toks_processed=}")
+
+    salt = output_bytes[:16]
+    kdf = Argon2id(salt=salt, length=32, iterations=1, lanes=4, memory_cost=64 * 1024)
+    key = kdf.derive(password)
+
+    nonce = output_bytes[16:28]
+    aesgcmsiv = AESGCMSIV(key)
+    decoded_bytes = aesgcmsiv.decrypt(nonce, output_bytes[28:], associated_data)
+
     decoded = decoded_bytes.decode("utf-8")
 
-    print (f"{decoded=}")
-
+    print(f"{decoded=}")
 
 
 if __name__ == "__main__":
