6366d85f95bae56052215fd3953d712a7fc7c781
Shawn Zivontsis <shawnz@shawnz.org>
2025-01-22 00:13:30 -0500

    Remove log statements

--- src/textcoder/llm_model.py
+++ src/textcoder/llm_model.py
@@ -1,5 +1,3 @@
-import time
-
 import torch
 import transformers
 
@@ -12,16 +10,13 @@ class LLMModel:
         self._model = model
         self._special_token_ids = special_token_ids
         self._past_key_values = transformers.DynamicCache()
-        print(f"{time.time()}: started initial forward pass")
         self._logits = model(
             input_ids=initial_input, past_key_values=self._past_key_values
         ).logits[0, -1]
-        print(f"{time.time()}: done initial forward pass")
         self._probs_dirty = True
         self._sorted_tok_ids_dirty = True
 
     def _recompute_probs(self):
-        print(f"{time.time()}: started recomputing probs")
         filtered_logits = self._logits
         filtered_logits[self._special_token_ids] = float("-inf")
         if _TOP_K == 0:
@@ -34,36 +29,28 @@ class LLMModel:
                 torch.softmax(topk_logits, dim=0),
                 topk_logit_indices,
             )
-        print(f"{time.time()}: sorted softmax logits")
         scaled_int_probs = torch.ceil(sorted_decimal_probs * _TARGET_PROB_ONE).int()
-        print(f"{time.time()}: scaled and rounded probs to ints")
         cumsum_probs = torch.cumsum(scaled_int_probs, dim=0)
-        print(f"{time.time()}: computed cumsum probs")
         truncation_point = torch.searchsorted(cumsum_probs, _TARGET_PROB_ONE)
         self._probs = cumsum_probs[: truncation_point + 1]
         self._prob_tok_ids = sorted_decimal_prob_indices[: truncation_point + 1]
         self._probs_dirty = False
-        print(f"{time.time()}: done recomputing probs")
 
     def _recompute_sorted_tok_ids(self):
-        print(f"{time.time()}: started recomputing sorted tok ids")
         if self._probs_dirty:
             self._recompute_probs()
         self._sorted_tok_ids, self._sorted_tok_id_indices = torch.sort(
             self._prob_tok_ids
         )
         self._sorted_tok_ids_dirty = False
-        print(f"{time.time()}: done recomputing sorted tok ids")
 
     def update(self, token):
-        print(f"{time.time()}: started update")
         self._logits = self._model(
             input_ids=torch.tensor([[token]], device=self._model.device),
             past_key_values=self._past_key_values,
         ).logits[0, -1]
         self._probs_dirty = True
         self._sorted_tok_ids_dirty = True
-        print(f"{time.time()}: done update")
 
     def get_sym_range(self, symbol):
         if self._sorted_tok_ids_dirty:
--- src/textcoder/textcoder.py
+++ src/textcoder/textcoder.py
@@ -1,6 +1,5 @@
 import io
 import os
-import time
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCMSIV
 from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
@@ -20,7 +19,7 @@ _PROMPT = [
 ]
 
 
-def compress(input_stream, output_stream, block_size=1):
+def compress(input_string, output_stream, block_size=1):
     tokenizer = AutoTokenizer.from_pretrained(_MODEL)
     model = AutoModelForCausalLM.from_pretrained(
         _MODEL, torch_dtype="auto", device_map="auto"
@@ -32,32 +31,15 @@ def compress(input_stream, output_stream, block_size=1):
     outbits = FOBitOutputStream(output_stream, block_size)
     encoder = ArithmeticEncoder(outbits)
 
-    input_tokens = tokenizer(input_stream)
-    input_toks_processed = []
+    input_tokens = tokenizer(input_string)
     for i, input_token in enumerate(input_tokens.input_ids):
         if i == 0 and input_token == tokenizer.bos_token_id:
-            print(f"{time.time()}: skipping bos token")
             continue
-
-        print(
-            f"{time.time()}: encoding token {input_token} ({repr(tokenizer.decode([input_token]))}, #{i + 1} of {len(input_tokens.input_ids)})"
-        )
         encoder.encode(model, input_token, True)
-        print(
-            f"{time.time()}: encoded token {input_token} ({repr(tokenizer.decode([input_token]))})"
-        )
-
-        input_toks_processed.append(input_token)
-        print(
-            f"{time.time()}: input processed so far: {repr(tokenizer.decode(input_toks_processed))}"
-        )
         model.update(input_token)
-        print(f"{time.time()}: updated model")
-
+        
     encoder.end()
     outbits.end()
-
-    return input_toks_processed
 
 
 def decompress(input_stream, block_size=1):
@@ -72,80 +54,42 @@ def decompress(input_stream, block_size=1):
     decoder = ArithmeticDecoder(FOBitInputStream(input_stream, block_size))
 
     output_toks = []
-    i = 0
     while True:
-        print(f"{time.time()}: decoding token #{i}")
-        i += 1
         sym = decoder.decode(model, True)
-        print(f"{time.time()}: decoded token {sym}")
         if sym < 0:
             break
-        print(f"{time.time()}: token {sym} has text {repr(tokenizer.decode([sym]))}")
-
-        print(
-            f"{time.time()}: ingested {input_stream.tell()} bytes of {len(input_stream.getvalue())}"
-        )
-
         output_toks.append(sym)
-        print(
-            f"{time.time()}: complete output so far: {repr(tokenizer.decode(output_toks))}"
-        )
         model.update(sym)
-        print(f"{time.time()}: updated model")
 
-    return output_toks, tokenizer.decode(output_toks)
+    return tokenizer.decode(output_toks)
 
 
 def main():
-    print(f"{time.time()}: started script")
-
     password = b"password"
     plaintext = b"hello world"
     associated_data = None
     salt = os.urandom(16)
     nonce = salt[:12]
-
     kdf = Argon2id(salt=salt, length=32, iterations=1, lanes=4, memory_cost=64 * 1024)
     key = kdf.derive(password)
-
     aesgcmsiv = AESGCMSIV(key)
     ct = aesgcmsiv.encrypt(nonce, plaintext, associated_data)
     input_bytes = salt + ct
 
-    print(f"input_bytes=0x{input_bytes.hex()}")
-
-    output_toks, encoded = decompress(io.BytesIO(input_bytes))
-
-    print(f"input_bytes=0x{input_bytes.hex()}")
-    print(f"{output_toks=}")
-
-    print(f"{time.time()}: decompressed input")
-
-    print(f"{encoded=}")
+    encoded = decompress(io.BytesIO(input_bytes))
 
     output_stream = io.BytesIO()
-    input_toks_processed = compress(encoded, output_stream)
-
-    print(f"{time.time()}: compressed output")
+    
+    compress(encoded, output_stream)
 
     output_bytes = output_stream.getvalue()
-
-    print(f"{encoded=}")
-    print(f" input_bytes=0x{input_bytes.hex()}")
-    print(f"output_bytes=0x{output_bytes.hex()}")
-    print(f"         {output_toks=}")
-    print(f"{input_toks_processed=}")
-
     salt = output_bytes[:16]
     kdf = Argon2id(salt=salt, length=32, iterations=1, lanes=4, memory_cost=64 * 1024)
     key = kdf.derive(password)
-
     nonce = output_bytes[:12]
     aesgcmsiv = AESGCMSIV(key)
-
     ct = output_bytes[16:]
     decoded_bytes = aesgcmsiv.decrypt(nonce, ct, associated_data)
-
     decoded = decoded_bytes.decode("utf-8")
 
     print(f"{decoded=}")
