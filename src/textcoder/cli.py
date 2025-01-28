e8a5b0d8a7e5c30783217a88d29db82cd5226cc7
Shawn Zivontsis <shawnz@shawnz.org>
2025-01-28 10:47:53 -0500

    Add --no-acceleration option

--- src/textcoder/cli.py
+++ src/textcoder/cli.py
@@ -8,7 +8,7 @@ import sys
 from cryptography.exceptions import InvalidTag
 from torch import OutOfMemoryError
 
-from textcoder.coding import decode, encode
+from textcoder.coding import LLMArithmeticCoder
 from textcoder.crypto import decrypt, encrypt
 
 _logger = logging.getLogger(__name__)
@@ -29,6 +29,12 @@ def main():
         "--no-validate",
         action="store_true",
         help="disable validation of encoded message",
+    )
+    parser.add_argument(
+        "-a",
+        "--no-acceleration",
+        action="store_true",
+        help="disable hardware acceleration",
     )
     parser.add_argument(
         "-d",
@@ -53,13 +59,14 @@ def main():
     password = (
         args.password if args.password is not None else getpass.getpass()
     ).encode()
+    coder = LLMArithmeticCoder(accelerated=not args.no_acceleration)
     if not args.decode:
         _logger.info("encoding starting")
         try:
             stdin_bytes = sys.stdin.buffer.read()
             encrypted_bytes = encrypt(password, stdin_bytes)
             with io.BytesIO(encrypted_bytes) as input_stream:
-                stegotext = decode(input_stream)
+                stegotext = coder.decode(input_stream)
         except OutOfMemoryError:
             _logger.error(
                 "ran out of memory while encrypting message", exc_info=args.verbose
@@ -73,7 +80,7 @@ def main():
             try:
                 gc.collect()
                 with io.BytesIO() as validation_stream:
-                    encode(stegotext, validation_stream)
+                    coder.encode(stegotext, validation_stream)
                     validation_bytes = validation_stream.getvalue()
                 decrypt(password, validation_bytes)
             except OutOfMemoryError:
@@ -101,7 +108,7 @@ def main():
         try:
             stdin_str = sys.stdin.read().removesuffix("\n")
             with io.BytesIO() as output_stream:
-                encode(stdin_str, output_stream)
+                coder.encode(stdin_str, output_stream)
                 output_bytes = output_stream.getvalue()
             plaintext = decrypt(password, output_bytes)
             sys.stdout.buffer.write(plaintext)
--- src/textcoder/coding.py
+++ src/textcoder/coding.py
@@ -16,52 +16,60 @@ _INITIAL_CONVERSATION = [
 ]
 
 
-def _get_tokenizer_and_arithmetic_model():
-    tokenizer = AutoTokenizer.from_pretrained(_HF_MODEL_NAME)
-    hf_model = AutoModelForCausalLM.from_pretrained(
-        _HF_MODEL_NAME, torch_dtype="auto", device_map="auto"
-    )
-    initial_input = tokenizer.apply_chat_template(
-        _INITIAL_CONVERSATION,
-        add_generation_prompt=True,
-        return_tensors="pt",
-        return_dict=True,
-    ).to(hf_model.device)  # type: ignore
-    arithmetic_model = LLMArithmeticModel(
-        hf_model,
-        initial_input,  # type: ignore
-        list(tokenizer.added_tokens_decoder.keys()),
-    )
-    return tokenizer, arithmetic_model
+class LLMArithmeticCoder:
+    def __init__(self, accelerated: bool = True, block_size: int = 1):
+        self._accelerated = accelerated
+        self._block_size = block_size
 
+    def _get_tokenizer_and_arithmetic_model(self):
+        tokenizer = AutoTokenizer.from_pretrained(_HF_MODEL_NAME)
+        hf_model_options = {"device_map": "auto"} if self._accelerated else {}
+        hf_model = AutoModelForCausalLM.from_pretrained(
+            _HF_MODEL_NAME, torch_dtype="auto", **hf_model_options
+        )
+        initial_input = tokenizer.apply_chat_template(
+            _INITIAL_CONVERSATION,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            return_dict=True,
+        ).to(hf_model.device)  # type: ignore
+        arithmetic_model = LLMArithmeticModel(
+            hf_model,
+            initial_input,  # type: ignore
+            list(tokenizer.added_tokens_decoder.keys()),
+        )
+        return tokenizer, arithmetic_model
 
-def encode(input_string: str, output_stream: typing.BinaryIO, block_size: int = 1):
-    tokenizer, arithmetic_model = _get_tokenizer_and_arithmetic_model()
-    outbits = FOBitOutputStream(output_stream, block_size)
-    encoder = ArithmeticEncoder(outbits)
+    def encode(
+        self,
+        input_string: str,
+        output_stream: typing.BinaryIO,
+    ):
+        tokenizer, arithmetic_model = self._get_tokenizer_and_arithmetic_model()
+        outbits = FOBitOutputStream(output_stream, self._block_size)
+        encoder = ArithmeticEncoder(outbits)
 
-    input_tokens = tokenizer(input_string)
-    for i, input_token in enumerate(input_tokens.input_ids):
-        if i == 0 and input_token == tokenizer.bos_token_id:
-            # skip bos token
-            continue
-        encoder.encode(arithmetic_model, input_token, True)
-        arithmetic_model.update(input_token)
+        input_tokens = tokenizer(input_string)
+        for i, input_token in enumerate(input_tokens.input_ids):
+            if i == 0 and input_token == tokenizer.bos_token_id:
+                # skip bos token
+                continue
+            encoder.encode(arithmetic_model, input_token, True)
+            arithmetic_model.update(input_token)
 
-    encoder.end()
-    outbits.end()
+        encoder.end()
+        outbits.end()
 
+    def decode(self, input_stream: typing.BinaryIO) -> str:
+        tokenizer, arithmetic_model = self._get_tokenizer_and_arithmetic_model()
+        decoder = ArithmeticDecoder(FOBitInputStream(input_stream, self._block_size))
 
-def decode(input_stream: typing.BinaryIO, block_size: int = 1) -> str:
-    tokenizer, arithmetic_model = _get_tokenizer_and_arithmetic_model()
-    decoder = ArithmeticDecoder(FOBitInputStream(input_stream, block_size))
+        output_toks = []
+        while True:
+            sym = decoder.decode(arithmetic_model, True)
+            if sym < 0:
+                break
+            output_toks.append(sym)
+            arithmetic_model.update(sym)
 
-    output_toks = []
-    while True:
-        sym = decoder.decode(arithmetic_model, True)
-        if sym < 0:
-            break
-        output_toks.append(sym)
-        arithmetic_model.update(sym)
-
-    return tokenizer.decode(output_toks)
+        return tokenizer.decode(output_toks)
